db4503f000b541256319c205bc5e22e43585288f
laubbran <bjlauber@uwm.edu>
2021-04-17 16:11:21 -0500

    Instructor child class created. Init, setter, and getter written.

---
+++ Instructor.py
@@ -0,0 +1,11 @@
+class Instructor(object):
+    """instructor child class from Account ABC"""
+
+    def __init__(self):
+        self.role = "instructor"
+
+    def getRole(self):
+        return self.role
+
+    def setRole(self, role):
+        self.role = role
