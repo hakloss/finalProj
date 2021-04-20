325d2381e00615a5a1c26f5ac117b78f4d013c92
hakloss <haleykloss@gmail.com>
2021-04-20 11:55:24 -0500

    outline

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.views import View
+from .models import XXX
 
 # Create your views here.
