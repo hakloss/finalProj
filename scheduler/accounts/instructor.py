149fad38d6dd1614b0fc127c29b1929df0d2854d
bjlauber <60158657+bjlauber@users.noreply.github.com>
2021-04-22 16:18:05 -0500

    Update instructor.py

--- scheduler/accounts/instructor.py
+++ scheduler/accounts/instructor.py
@@ -1,12 +1,57 @@
-class Instructor(Account):
+import account
+
+
+class Instructor(account):
     """instructor child class from Account ABC"""
 
     def __init__(self):
-        self.role = "instructor"
-
-    def getRole(self):
-        return self.role
+        self.role = "Instructor"
+        self.name = None
+        self.email = None
+        self.address = None
+        self.streetName = None
+        self.zip = None
+        self.state = None
+        self.phoneNumber = None
 
     def setRole(self, role):
         self.role = role
 
+    def getRole(self):
+        return self.role
+
+    def setName(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    def setEmail(self, email):
+        self.email = email
+
+    def getEmail(self):
+        return self.email
+
+    def setAddress(self, address, streetName, zip, state):
+        self.address = address
+        self.streetName = streetName
+        self.zip = zip
+        self.state = state
+
+    def getAddress(self):
+        return self.address
+
+    def getStreetName(self):
+        return self.streetName
+
+    def getZip(self):
+        return self.zip
+
+    def getState(self):
+        return self.state
+
+    def setPhone(self, phoneNumber):
+        self.phoneNumber = phoneNumber
+
+    def getPhone(self):
+        return self.phoneNumber
