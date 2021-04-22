f34b9e4ffbdbbf5d9915d55ce9ebcf88fe8c7f23
bjlauber <60158657+bjlauber@users.noreply.github.com>
2021-04-22 16:19:49 -0500

    Update tests.py

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 from courses import*
+from scheduler.accounts.instructor import Instructor
 
 
 class TestCourseinit(unittest.TestCase):
@@ -33,6 +34,80 @@ class TestCourselistSections(unittest.TestCase):
         self.temp.addSection(x)
         self.temp.addSection(y)
         self.temp.addSection(z)
+        
+class TestInstructorInit(unittest.TestCase):
+    # test creation
+    def setUp(self):
+        self.a = Instructor()
+
+    def test_default(self):
+        self.assertEqual(self.a.getRole(), "Instructor", msg="default broke; role")
+        self.assertEqual(self.a.getName(), None, msg="default broke; name")
+        self.assertEqual(self.a.getEmail(), None, msg="default broke; email")
+        self.assertEqual(self.a.getAddress(), None, msg="default broke; address")
+        self.assertEqual(self.a.getStreetName(), None, msg="default broke; street name")
+        self.assertEqual(self.a.getZip(), None, msg="default broke; zip code")
+        self.assertEqual(self.a.getState(), None, msg="default broke; state")
+        self.assertEqual(self.a.getPhone(), None, msg="default broke; phone number")
+
+
+class TestRole(unittest.TestCase):
+    # test role attribute
+    def setUp(self):
+        self.a = Instructor()
+        self.a.setRole("Administrator")
+
+    def test_role(self):
+        self.assertEqual(self.a.getRole(), "Administrator", msg="role failed")
+
+
+class TestName(unittest.TestCase):
+    # test name attribute
+    def setUp(self):
+        self.a = Instructor()
+        self.a.setName("John")
+
+    def test_name(self):
+        self.assertEqual(self.a.getName(), "John", msg="name failed")
+
+
+class TestEmail(unittest.TestCase):
+    # test email attribute
+    def setUp(self):
+        self.a = Instructor()
+        self.a.setEmail("foo.com")
+
+    def test_email(self):
+        self.assertEqual(self.a.getEmail(), "foo.com", msg="email failed")
+
+
+class TestAddress(unittest.TestCase):
+    # test address attribute
+    def setUp(self):
+        self.a = Instructor()
+        self.a.setAddress("1234", "Kenwood Boulevard", "54321", "Wisconsin")
+
+    def test_address(self):
+        self.assertEqual(self.a.getAddress(), "1234", msg="address failed")
+
+    def test_street_name(self):
+        self.assertEqual(self.a.getStreetName(), "Kenwood Boulevard", msg="street name failed")
+
+    def test_zip_code(self):
+        self.assertEqual(self.a.getZip(), "54321", msg="zip code failed")
+
+    def test_state(self):
+        self.assertEqual(self.a.getState(), "Wisconsin", msg="state failed")
+
+
+class TestPhone(unittest.TestCase):
+    # test phone attribute
+    def setUp(self):
+        self.a = Instructor()
+        self.a.setPhone("1234567890")
+
+    def test_phone(self):
+        self.assertEqual(self.a.getPhone(), "1234567890", msg="phone failed")
 
     def test_listSection(self):
         self.assertEqual(self.temp.listSections(), ["201, MWF, 1-3", "202, TR, 2-10", "203, T, 1-2"])
