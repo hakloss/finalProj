82b80b36bb30e6309bd2b731b84608663b786950
Ryan Nyeck <frn@uwm.edu>
2021-04-23 21:58:12 -0500

    Acceptance

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,6 +1,104 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import TestCase, Client
+from  .models import accounts ,Administrator, Instructor, TA, Courses
 
 # Create your tests here.
+class LoginList(TestCase):
+    client = None
+    courses = None
+
+    def setUp(self):
+        self.client = Client()
+        self.courses = {"CS361":["Intro to software engineering"], "CS337":["System Programming"]}
+
+        for i in self.courses.keys():
+            temp = Client(name = i, password = i)
+            temp.save()
+            for j in self.courses[i]:
+                Courses(name = j, owner = temp).save()
+
+    def test_CorrectName(self):
+        for i in self.courses.keys():
+            resp = self.client.post("/", {"name":i, "password":i}, follow = True)
+            self.assertEqual(resp.context["name"],i , "Name was not passed from the Login list")
+
+
+    def test_complete(self):
+        for i in self.courses.keys():
+            resp = self.client.post("/", {"name":i, "password":i}, follow = True)
+            for j in self.courses[i]:
+                self.assertIn(resp.context["course"], i, "A course is missing, for:" + i)
+
+    def test_noExtra(self):
+        for i in self.courses.keys():
+            resp = self.client.post("/",{"name":i, "password":i}, follow = True)
+            for i self.courses[i]:
+                self.assertIn(j,self.courses[i], "There is one extra course, user:" + i)
+
+
+class Login_success(TestCase):
+    client = None
+    courses = None
+
+    def setUp(self):
+        self.client = Client()
+        self.client = {"CS361":["Intro to software engineering"], "CS337":["System Programming"]}
+
+        for i in self.courses.keys():
+            temp = Client(name = i, password = i)
+            temp.save()
+            for j in self.courses[i]
+                Courses(name = j, owner = temp).save()
+
+    def test_validLogin(self):
+        resp = self.client.post("/", {"name": "one", "password":"one"}, follow = True)
+        self.assertEqual(resp.status_code, 200) #Ok
+
+class LoginFail(TestCase):
+    client = None
+    courses = None
+
+    def setUp(self):
+        self.client = Client()
+        self.client = {"CS361": ["Intro to software engineering"], "CS337": ["System Programming"]}
+
+        for i in self.courses.keys():
+            temp = Client(name=i, password=i)
+            temp.save()
+            for j in self.courses[i]
+                Courses(name=j, owner=temp).save()
+
+    def test_noPassword(self):
+        resp = self.client.post("/",{"name": "admin", "password": "instructor"}, follow = True)
+        self.assertEqual(resp.context["message"],"bad password", "Failed login password, ,user: admin, password: instructor")
+
+    def test_badPassword(self):
+        resp = self.client.post("/",{"name": "admin", "password": "ta"}, follow = True)
+        self.assertEqual(resp.context["message"],"bad password", "Failed login password, user: admin, password: ta, ta valid for another user")
+
+
+
+class CreateAccount(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def accountcreated(self):
+        pass
+
+    def noneforemptyfields(self):
+        pass
+
+    def duplicateuser(self):
+        pass
+
+class CreateCourse(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def coursecreated(self):
+        pass
+
+    def duplicatecourse(self):
+        pass
