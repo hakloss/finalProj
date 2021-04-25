5574056f11770ec3677b06ee546523e9f377726b
hakloss <haleykloss@gmail.com>
2021-04-25 11:19:09 -0500

    copied urls and models into this branch

--- scheduler/models.py
+++ scheduler/models.py
@@ -4,5 +4,38 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class XXX(models.Model):
-    pass
+class user(models.Model):
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    #  0 = admin   |   1 = instructor   |   2 = TA
+    role = models.IntegerField()
+    address = models.CharField(max_length=40)
+    city = models.CharField(max_length=40)
+    state = models.CharField(max_length=2)
+    zip = models.CharField(max_length=5)
+    pphone = models.CharField(max_length=5)
+    wphone = models.CharField(max_length=5)
+
+class course(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class section(models.Model):
+    number = models.IntegerField()
+    lectureTime = models.CharField(max_length=30)
+    course = models.ForeignKey(course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.number
+
+class lab(models.Model):
+    number = models.IntegerField()
+    labTime = models.CharField(max_length=30)
+    section = models.ForeignKey(section, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.number
--- scheduler/views.py
+++ scheduler/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from django.views import View
-from .models import XXX
+from scheduler.models import user, course, section, lab
 
 # Create your views here.
 
@@ -11,34 +11,35 @@ from .models import XXX
 # Create your views here.
 class Login(View):
     def get(self, request):
-        return render(request,"home.html",{})
+        return render(request,"home.html",{"user":user.email,"password":user.password})
+        #return render(request, "login.html")
 
     def post(self, request):
-        noSuchUser = False
-        badPassword = False
-        try:
-            m = XXX.objects.get(name=request.POST['name'])
-            badPassword = (m.password != request.POST['password'])
-        except:
-            noSuchUser = True
-        if noSuchUser:
-            return render(request,"home.html",{"invalidmsg":"User does not exist"})
-        elif badPassword:
-            return render(request,"home.html",{"invalidmsg":"Please enter valid password"})
-        else:
-            request.session["name"] = m.name
-            return redirect("/home/")
-
+        # noSuchUser = False
+        # badPassword = False
+        # try:
+        #     m = user.objects.get(name=request.POST['name'])
+        #     badPassword = (m.password != request.POST['password'])
+        # except:
+        #     noSuchUser = True
+        # if noSuchUser:
+        #     return render(request,"home.html",{"invalidmsg":"User does not exist"})
+        # elif badPassword:
+        #     return render(request,"home.html",{"invalidmsg":"Please enter valid password"})
+        # else:
+        #     request.session["name"] = m.name
+        #     return redirect("/home/")
+        return render(request, "login.html")
 
 class Home(View):
     def get(self, request):
-        m = request.session["name"]
+        #m = request.session["name"]
         #if m doesn't have a name
         #This is account validation and should become a function
         #return redirect("/login/")
-
+        return render(request,"home.html")
     def post(self, request):
-        pass
+        return render(request,"home.html")
 
 class CreateAccount(View):
     def get(self, request):
@@ -46,10 +47,12 @@ class CreateAccount(View):
         # if m doesn't have a name
         # This is account validation and should become a function
         # return redirect("/login/")
-        return render(request, "CreateAccount.html", {"fname": XXX, "lname": XXX, "email":XXX, "password":XXX,
-                                                      "role":XXX, "address":XXX, "city":XXX, "state":XXX, "zip":XXX, "pphone":XXX,
-                                                      "wphone":XXX})
 
+        # return render(request, "CreateAccount.html", {"fname": user.fname, "lname": user.lname, "email":user.email, "password":user.password,
+        #                                               "role":user.role, "address":user.address, "city":user.city, "state":user.state,
+        #                                               "zip":user.zip, "pphone":user.pphone,"wphone":user.wphone})
+
+        return render(request, "CreateAccount.html")
 
     def post(self, request):
         xfname = request.POST.get('fname')
@@ -64,15 +67,15 @@ class CreateAccount(View):
         xpphone=request.POST.get('pphone')
         xwphone=request.POST.get('wphone')
 
-        account = XXX(fname=xfname, lname=xlname,email=xemail, password=xpassword, role=xrole, address=xaddress,
-                      city=xcity, state=xstate,zip=xzip,pphone=xpphone, wphone=xwphone)
+        account = user(fname=xfname, lname=xlname,email=xemail, password=xpassword, role=xrole,
+                       address=xaddress,city=xcity, state=xstate,zip=xzip,pphone=xpphone, wphone=xwphone)
         account.save()
 
         return render(request, "CreateAccount.html", {"successmsg": "Account has been created"})
-
+        #return render(request, "CreateAccount.html")
 class CreateCourse(View):
     def get(self, request):
-        pass
+        return render(request, "CreateCourse.html")
 
     def post(self, request):
-        pass
+        return render(request, "CreateCourse.html")
